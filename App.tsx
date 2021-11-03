65814a72bca5644ea9ea5f374ac46478bb800b12
poteboy <keitatkins4@gmail.com>
2021-11-04 01:03:42 +0900

    create drawer

--- App.tsx
+++ App.tsx
@@ -3,18 +3,9 @@ import {AppNavigator} from '@src/navigation/navigator';
 import {NavigationContainer} from '@react-navigation/native';
 import {NativeBaseProvider} from 'native-base';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import {Button, View} from 'react-native';
 import '@src/constants/langulage/i18n';
 
 const Drawer = createDrawerNavigator();
-
-function NotificationsScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
 
 export default function App() {
   return (
@@ -23,7 +14,6 @@ export default function App() {
         {/* <AppNavigator /> */}
         <Drawer.Navigator>
           <Drawer.Screen name={'tab'} component={AppNavigator} />
-          <Drawer.Screen name={'test'} component={NotificationsScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
